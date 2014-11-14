4a66bcd3facba4d938d1f3977c786ce24b51e82b
Jonti Harris <jonathan.harris2@mail.mcgill.ca>
2014-11-14 18:57:59 -0500

    working!~
    almost finished q1

--- a3q1.c
+++ a3q1.c
@@ -1,30 +1,42 @@
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-const char* getfield(char* line, int num)
-{
-    const char* tok;
-    for (tok = strtok(line, ";");
-            tok && *tok;
-            tok = strtok(NULL, ";\n"))
-    {
-        if (!--num)
-            return tok;
-    }
-    return NULL;
-}
 
-int main()
+int main(int argc, char *argv[])
 {
-    FILE* stream = fopen("input", "r");
+	FILE *file_ptr;
+	char *token;
+	int value;
+	int count;
+	char line[1024];
 
-    char line[1024];
-    while (fgets(line, 1024, stream))
-    {
-        char* tmp = strdup(line);
-        printf("Field 3 would be %s\n", getfield(tmp, 3));
-        // NOTE strtok clobbers tmp
-        free(tmp);
-    }
+	file_ptr = fopen(argv[1], "r");
+
+
+	if (file_ptr == NULL)
+		{
+			printf("Error");
+			return 1;
+		}
+
+
+	while(!feof(file_ptr))
+	{
+			count = 0;
+			value = 0;
+			fgets(line,299,file_ptr);
+			token = strtok(line,",");
+			while (token != NULL)
+  			{
+   			 	value = value + atoi(token);
+				count++;
+    			token = strtok(NULL, ",");
+  			}
+			printf("%i\n", value/count);
+
+	}
+
+	fclose(file_ptr);
+	return 0;
 }
